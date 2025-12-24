2f83a1bb9ad24d47f3bb95f959d6106949dde297
Ahadmoen <ahadmoeeen@gmail.com>
2025-12-25 01:13:48 +0500

    spark applied

--- packages/__init__.py
+++ packages/__init__.py
@@ -1,2 +1,4 @@
 from .load import Extract
 from .graphs import histplot
+from .graphs import boxplot 
+from .graphs import violin
--- packages/graphs.py
+++ packages/graphs.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def histplot(x):
     histplot(x=x)
+
+
+def boxplot(x):
+    sns.boxplot(x=x, color='lightblue')
+
+
+def violin(data, x, y, hue):
+    sns.violinplot(
+    data=data,
+    y=x,
+    x=y,
+    hue=hue
+)
